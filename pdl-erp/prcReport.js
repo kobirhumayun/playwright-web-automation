48b1e01950c8ca46497331ab6ce97219ca44036c
Humayun <kobirhumayun@gmail.com>
2025-02-16 00:36:12 +0600

    feat: add module prcReport

---
+++ pdl-erp/prcReport.js
@@ -0,0 +1,34 @@
+const { chromium } = require('playwright');
+
+async function downloadPrcReport() {
+    const browser = await chromium.launch({ headless: false }); // Set headless to false for visible browser
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    // await page.pause();
+
+    try {
+        await page.goto('https://pdlerpweb.pioneerdenim.com/RptCustoms/PRCReport');
+        await page.getByRole('textbox', { name: 'Select LC Date' }).click();
+        await page.getByRole('option', { name: 'Last Year' }).click();
+        await page.locator('.dxrd-right-tabs').click();
+        await page.locator('div:nth-child(2) > .dx-dropdowneditor-input-wrapper > .dx-texteditor-container > .dx-texteditor-input-container').click();
+        await page.locator('.dx-list-select-all > .dx-widget').click();
+        await page.locator('.dxrd-right-tabs').click();
+        await page.getByRole('button', { name: 'Submit' }).click();
+        await page.getByRole('menuitem', { name: '' }).locator('div').nth(4).click();
+        const downloadPromise = page.waitForEvent('download');
+        await page.getByText('XLSX', { exact: true }).click();
+        const download = await downloadPromise;
+        await download.saveAs('PrcReport.xlsx');
+
+        console.log('XLSX saved successfully!');
+
+    } catch (error) {
+        console.error('Error during automation:', error);
+    } finally {
+        await browser.close();
+    }
+}
+
+downloadPrcReport();
+
