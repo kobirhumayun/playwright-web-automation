e88fe7aa889606628713759094a9d7e78c3eb939
Humayun <kobirhumayun@gmail.com>
2025-02-17 23:19:58 +0600

    fix: modify downloadPrcReport
    Increase download menu delay time

--- pdl-erp/function-definition/prcReport.js
+++ pdl-erp/function-definition/prcReport.js
@@ -10,7 +10,7 @@ async function downloadPrcReport(url, fileName, fileType, downloadPath) {
     try {
 
         const startTime = new Date().getTime();
-        
+
         await page.goto(url);
         await page.getByRole('textbox', { name: 'Select LC Date' }).click();
         await page.getByRole('option', { name: 'Last Year' }).click();
@@ -20,15 +20,15 @@ async function downloadPrcReport(url, fileName, fileType, downloadPath) {
         await page.locator('.dxrd-right-tabs').click();
         await page.getByRole('button', { name: 'Submit' }).click();
         const menuItem = await page.getByRole('menuitem', { name: '' }).locator('div').nth(4);
-        await menuItem.waitFor({ state: 'visible' });
+        await menuItem.waitFor({ state: 'visible', timeout: 300000 });
         await menuItem.click();
         const downloadPromise = page.waitForEvent('download', { timeout: 300000 });
         await page.getByText(fileType.toUpperCase(), { exact: true }).click();
         const download = await downloadPromise;
         const filePath = path.join(downloadPath, `${fileName}.${fileType.toLowerCase()}`);
-        
+
         await download.saveAs(filePath);
-        
+
         console.log(`${fileType.toUpperCase()} saved successfully as ${fileName}.${fileType.toLowerCase()}`);
         console.log('Time taken:', (new Date().getTime() - startTime) / 1000, 'seconds');
 
